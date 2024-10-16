4e04c1eccea0664d4dce18dd4cc2db73482d5f06
Abdulrahman-Al-Hasbani-UCLL <Abdulrahman.alhasbani@student.ucll.be>
2024-10-16 13:27:58 +0200

    Project set up

---
+++ Restaurant_manager_x_Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Restaurant_manager_x_backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
